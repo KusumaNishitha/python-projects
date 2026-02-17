e65600f9e7f7189fc370ad8486fb4d4285f54697
KusumaNishitha <kusumanishitha14@gmail.com>
2026-02-17 20:59:07 +0530

    Add files via upload

---
+++ py/password.py
@@ -0,0 +1,6 @@
+import random
+password ="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@$%#^&*()_+=}{:"
+length_password = int(input("enter the length of the password: "))
+a = "".join(random.sample(password,length_password))
+print(f"your password is {a}")
+
